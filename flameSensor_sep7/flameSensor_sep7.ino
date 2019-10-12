8d520bf78ae1a934f2d9f7b14c407ea6db91e981
krishnatcs17 <krishnat.cs17@bmsce.ac.in>
2019-10-12 12:16:33 +0530

    oct12

---
+++ flameSensor_sep7/flameSensor_sep7.ino
@@ -0,0 +1,16 @@
+int fIn = A3;
+int lOut = 4;
+int buzz = 11;
+int fval;
+
+void setup() {
+  
+  pinMode(fIn, INPUT);
+  pinMode(lOut, OUTPUT);
+  pinMode(buzz, OUTPUT);
+}
+
+void loop() {
+  
+  
+}
